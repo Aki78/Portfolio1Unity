c63b4e8b1d955196f9328fd6e380fd230e1dae5b
Aki78 <kmorooka78@gmail.com>
2023-10-28 17:18:56 +0300

    done multiple enemies

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,6 +2,27 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour{
 
+
+    public float frequency = 1.0f;
+    public float magnitude = 5.5f;
+    public float downwardSpeed = 0.05f;
+
+    private Vector3 startPos;
+    private float timeCounter = 0;
+
+    void Start() {
+        startPos = transform.position;
+    }
+
+    void Update() {
+        timeCounter += Time.deltaTime * frequency;
+
+        float x = Mathf.Sin(timeCounter) * magnitude;
+        float y = startPos.y - downwardSpeed * Time.time;
+
+        transform.position = new Vector3(startPos.x + x, y, transform.position.z);
+    }
+
     void OnCollisionEnter2D(Collision2D collision){
         BulletBehaviour bullet = collision.gameObject.GetComponent<BulletBehaviour>();
 
